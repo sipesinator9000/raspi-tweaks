1741e3a40182e74e0f501d692047a97bacebddb3
Alex Sipes <alexjsipes@gmail.com>
2025-10-11 22:40:56 +0100

    formatting

--- performance_test.sh
+++ performance_test.sh
@@ -40,7 +40,9 @@ sleep 5s
 
 ## Run sysbench test and append the output to the log file
 echo "Running stress test..."
-echo "=== Stress test started at $(date) ===" >> $LOG_FILE
+echo "
+=== Stress test started at $(date) ===
+" >> $LOG_FILE
 sysbench --test=cpu --cpu-max-prime=20000 --num-threads=4 run >> $LOG_FILE
 STRESS_EXIT_CODE=$?
 echo "=== Stress test finished at $(date) ===" >> $LOG_FILE
