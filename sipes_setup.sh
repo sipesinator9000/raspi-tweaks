c64bb08c79b915e910aaf3bdb469eabab1aea63d
Alex Sipes <alexjsipes@gmail.com>
2025-10-11 17:05:00 +0100

    added initial install script

---
+++ sipes_setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Expand filesystem
+sudo raspi-config --expand-rootfs
+
+# Update raspi things
+sudo apt-get update
+sudo apt-get dist-upgrade
+sudo apt-get upgrade
+
+# Install Retropie
+cd ~
+git clone --depth=1 https://github.com/RetroPie/RetroPie-Setup.git
+cd RetroPie-Setup
+sudo ./retropie_setup.sh
+
+# Install additional workflow tools
+sudo apt-get install vim
+sudo apt-get install silversearcher-ag
+
+# Install testing tools
+sudo apt-get install iostat
+sudo apt-get install sysbench
+sudo apt-get install xclip
+
+# Install Retroflag NesPi safe shutdown script
+cd ~
+wget -O - "https://raw.githubusercontent.com/RetroFlag/retroflag-picase/master/install_gpi.sh" | sudo bash
+
+exit
