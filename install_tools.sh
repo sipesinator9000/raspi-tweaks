2a19a31f9d1acfc6dd061f07dc8bc62d5fdd9566
Alex Sipes <alexjsipes@gmail.com>
2025-10-15 11:32:41 +0100

    added loads of comments to install tools

--- install_tools.sh
+++ install_tools.sh
@@ -10,18 +10,18 @@ sudo apt-get install python-dev python-pip python-gpiozero
 sudo pip install psutil pyserial
 
 # Install additional workflow tools
-sudo apt-get install vim
-sudo apt-get install emacs
-sudo apt-get install gh
-sudo apt-get install silversearcher-ag
-sudo apt-get install tmux
-sudo apt-get install sl
+sudo apt-get install vim                # 1st fav text editor
+sudo apt-get install emacs              # 2nd fav text editor
+sudo apt-get install gh                 # github
+sudo apt-get install silversearcher-ag  # Great, fast search tool
+sudo apt-get install tmux               # Terminal multiplexer
+sudo apt-get install sl                 # Punishes the user for typing too quickly
 
 # Install testing tools
-sudo apt-get install iostat
-sudo apt-get install sysbench
-sudo apt-get install xclip
-sudo apt install nmon
+sudo apt-get install iostat    # Network stuff
+sudo apt-get install sysbench  # Stress testing and benchmarking tool
+sudo apt-get install xclip     # Pipe things to your clipboard
+sudo apt install nmon          # Excellent interactive system monitor
 
 # Install Retroflag NesPi safe shutdown script
 cd ~
