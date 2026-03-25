53790ae508908d1d805fd8b354cb815d71fcb713
Alex Sipes <alexjsipes@gmail.com>
2026-03-25 21:56:08 +0000

    reduced the prime number limit

--- performance_test.sh
+++ performance_test.sh
@@ -52,7 +52,7 @@ sleep 3
 echo "Running stress test..."
 echo "
 === Stress test started at $(date) ===" >> $LOG
-sysbench cpu --cpu-max-prime=50000 --threads=4 --time=300 run 2>&1 >> $LOG
+sysbench cpu --cpu-max-prime=10000 --threads=4 --time=300 run 2>&1 >> $LOG
 STRESS_EXIT_CODE=$?
 echo "
 === Stress test finished at $(date) ===" >> $LOG
