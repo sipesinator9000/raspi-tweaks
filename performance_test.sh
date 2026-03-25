d2bb78366e86a589dd9263cabd472b79facf7b1e
Alex Sipes <alexjsipes@gmail.com>
2026-03-25 21:49:18 +0000

    cleaned up the temp logger using new vars

--- performance_test.sh
+++ performance_test.sh
@@ -36,7 +36,7 @@ echo "=======================================
 ## Start temperature logging in the background
 log_temperature() {
     while true; do
-        echo "Time $(date +"%T"), CPU $(vcgencmd measure_temp), $(vcgencmd measure_volts), $(vcgencmd get_throttled), MEM $(free | awk '/^Mem/ {printf "%d%s",$3/$2*100,"%"}')" >> $LOG_FILE
+        echo "Time $TIME, CPU $CPU_TEMP, $CPU_VOLTS, $THROTTLED" >> $LOG
         sleep 1
     done
 }
